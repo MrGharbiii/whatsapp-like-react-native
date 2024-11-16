c4ee51793b93b5835cbd61af1588bbe9acdf7bc5
Ahmed Gharbi <ahmedgharbigharbi718@gmail.com>
2024-11-16 04:16:54 +0100

    set .env file

--- config/index.js
+++ config/index.js
@@ -1,24 +1,28 @@
-// Import the functions you need from the SDKs you need
 import app from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/database';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from '@env';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBSjKFiPAB_LB11uA1ibP2cQJBOB2jbGds',
-  authDomain: 'whatsapp-d2fb9.firebaseapp.com',
-  databaseURL: 'https://whatsapp-d2fb9-default-rtdb.firebaseio.com',
-  projectId: 'whatsapp-d2fb9',
-  storageBucket: 'whatsapp-d2fb9.firebasestorage.app',
-  messagingSenderId: '771408375685',
-  appId: '1:771408375685:web:c5fbe3febcf0ef43889eec',
-  measurementId: 'G-2K6GZJ9WVE',
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const firebase = app.initializeApp(firebaseConfig);
 
 export default firebase;
